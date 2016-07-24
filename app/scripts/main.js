8b0f4575e506ec9d7670ae640c4f625694425bb7
Khalid Sookia <khalidsookia@gmail.com>
2016-07-24 14:20:57 +0200

    Added config for routing, ng-view and seperated templates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-var blog = angular.module('blog', []); // jshint ignore:line
+var blog = angular.module('blog', ['ngRoute']); // jshint ignore:line
+
+blog.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'BlogListingCtrl',
+            templateUrl: 'templates/blogs.html'
+        }).when('/:id', {
+            controller: 'BlogShowCtrl',
+            templateUrl: 'templates/blog.html'
+        })
+    ;
+}]);
+
+blog.controller('BlogListingCtrl', ['$scope', function($scope) {
+}]);
+
+blog.controller('BlogShowCtrl', ['$scope', function($scope) {
+}]);
